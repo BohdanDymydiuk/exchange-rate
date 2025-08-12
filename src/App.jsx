72b045509b7d9adc637146e992a6ce14009dc43e
Bohdan Dymydiuk <b.v.dymydiuk@gmail.com>
2025-08-12 12:16:47 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <div className={styles.app}>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa adipisci placeat itaque beatae animi? Temporibus, reprehenderit. Natus exercitationem hic ad, eos harum rerum iusto similique ullam maiores alias. Sequi, iste!
+    </div>
+  )
+}
+
+export default App
